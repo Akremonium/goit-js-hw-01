7c35978e57fbab21010276e4c29667ec22a641a4
Akremonium <akremonium@gmail.com>
2021-02-11 19:42:55 +0200

    changes-name-of-variable

--- task-1.js
+++ task-1.js
@@ -1,7 +1,7 @@
 'use strict';
-const name = 'Генератор защитного поля';
+const item = 'Генератор защитного поля';
 let price = 1000;
 
-console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+console.log(`Выбран ${item}, цена за штуку ${price} кредитов.`);
 price = 2000;
-console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+console.log(`Выбран ${item}, цена за штуку ${price} кредитов.`);
